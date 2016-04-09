4b0c3a8cfa8e501f46d265b73027ababe9ba7bd2
Roman Ehrbar <r1ehrbar@hsr.ch>
2016-04-09 16:32:49 +0000

    bugfix, works now without root

--- subscriberLCD.py
+++ subscriberLCD.py
@@ -10,15 +10,16 @@ def on_connect(client, userdata, flags, rc):
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
-    with cursor(lcd, 1, 0):
-        lcd.write_string(str(msg.payload).ljust(4))
+    lcd.cursor_pos = (1,0)
+    # fill up the whole line with spaces to clear previous values.
+    lcd.write_string(str(msg.payload).ljust(16))
 
-lcd = CharLCD(pin_rs=4, pin_e=17, pins_data=[18, 22, 23, 24],
+lcd = CharLCD(pin_rs=25, pin_rw=None, pin_e=17, pins_data=[18, 22, 23, 24],
               numbering_mode=GPIO.BCM,
               cols=16, rows=2)
 
-with cleared(lcd):
-    lcd.write_string('Messwert:')
+lcd.clear()
+lcd.write_string('Messwert:')
 
 client = mqtt.Client()
 client.on_connect = on_connect
