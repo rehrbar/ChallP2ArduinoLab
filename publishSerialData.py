ae834506b9c27f95082ab85507cb0de5f1d1066e
Roman Ehrbar <r1ehrbar@hsr.ch>
2016-04-09 14:20:07 +0200

    updated serial reader

--- publishSerialData.py
+++ publishSerialData.py
@@ -1,4 +1,25 @@
+#!/usr/bin/python
 import serial
+import paho.mqtt.client as mqtt
+
+# callbacks
+def on_connect(client, userdata, flags, rc):
+    print("Connected to broker with result code " + str(rc))
+
+# initialize
 ser = serial.Serial('/dev/ttyACM0', 9600)
-while True:
-    print ser.readline()
+client = mqtt.Client()
+client.on_connect = on_connect
+
+client.connect("localhost", 1883)
+client.loop_start()
+
+try:
+    # publish loop
+    while True:
+        sensor_value = int(ser.readline())
+        print("Sensor value: " + sensor_value)
+        client.publish("arduino/poti", sensor_value)
+except KeyboardInterrupt:
+    print('^C received, shutting down client')
+    client.loop_stop()
