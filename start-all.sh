f4ef4c87e7456de2e0e0dfb33dbba95aa44f0d24
Roman Ehrbar <r1ehrbar@hsr.ch>
2016-04-10 18:44:04 +0000

    fixed start-all.sh

--- start-all.sh
+++ start-all.sh
@@ -1,12 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 python publishSerialData.py 1>/dev/null&
-client1 = $!
+client1=$!
 python subscriberLCD.py 1>/dev/null&
-client2 = $!
+client2=$!
 python subscriberLED.py 1>/dev/null&
-client3 = $!
+client3=$!
 python -m SimpleHTTPServer 8080 1>/dev/null&
-client4 = $!
+client4=$!
+
+function ctrl_c() {
+	kill $client1
+	kill $client2
+	kill $client3
+	kill $client4
+	exit
+}
+
+trap ctrl_c SIGHUP SIGINT SIGTERM
 
 echo "Press [CTRL+C] to stop.."
 while :
@@ -14,7 +24,3 @@ do
 	sleep 1
 done
 
-kill $client1
-kill $client2
-kill $client3
-kill $client4
